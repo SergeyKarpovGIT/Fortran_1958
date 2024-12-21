b322467238a066d7479a657ceb41eb7132bd81c1
Red-Eared Slider <new.message4you@gmail.com>
2024-12-21 22:22:43 +0300

    compilation modifications

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 gfortran14 -Wall -std=f2018 -fall-intrinsics -fimplicit-none \
+ -static-libgfortran -static-libquadmath \
  -o /var/tmp/math.out /home/slider/fortran/source.f08
